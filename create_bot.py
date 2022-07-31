2170b4e5c09638986c94df71a91b51856dec09cd
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-07-31 15:09:57 +0300

    fix README.md

--- create_bot.py
+++ create_bot.py
@@ -7,9 +7,13 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+import config
 
 storage = MemoryStorage()
 
-bot = Bot(token=os.getenv('TOKEN'))
+
+# bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=config.TOKEN)
+
 
 dp = Dispatcher(bot, storage=storage)
